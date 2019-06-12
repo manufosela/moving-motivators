95a239f1185139f05026f3020afec7cf467fee8d
Manuel Juan Fosela Águila <manufosela@gmail.com>
2019-06-12 04:39:37 +0200

    drag&drop and mobile actions

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,13 @@
 let dragged;
-
-/*
-el.addEventListener("touchstart", dragstart, false);
-el.addEventListener("touchend", dragend, false);
-//el.addEventListener("touchcancel", handleCancel, false);
-el.addEventListener("touchleave", dragleave, false);
-//el.addEventListener("touchmove", handleMove, false);
-*/
-
-function drag(event) {
-
-}
+let selectedEl;
 
 function dragstart(event) {
-  dragged = event.target; // store a ref. on the dragged elem
-  event.target.style.opacity = .5; // make it half transparent
+  dragged = event.target;
+  event.target.style.opacity = .5;
 }
 
 function dragend(event) {
-  event.target.style.opacity = ''; // reset the transparency
+  event.target.style.opacity = '';
 }
 
 function dragover(event) {
@@ -26,51 +15,83 @@ function dragover(event) {
 }
 
 function dragenter(event) {
-  // highlight potential drop target when the draggable element enters it
   if (event.target.className === 'dropzone') {
     event.target.style.background = 'purple';
   }
 }
 
 function dragleave(event) {
-  // reset background of potential drop target when the draggable element leaves it
   if (event.target.className === 'dropzone') {
     event.target.style.background = '';
   }
 }
 
 function drop(event) {
-  event.preventDefault(); // prevent default action (open as link for some elements)
-
-  // move dragged elem to the selected drop target
+  event.preventDefault();
   if (event.target.className === 'dropzone') {
     event.target.style.background = '';
+    dragged.className = '';
+    selectedEl = null;
     dragged.parentNode.removeChild(dragged);
     event.target.appendChild(dragged);
   }
 }
 
-function load() {
+function desktopOption() {
   const DaDZone = document.getElementById('draganddropzone');
-
   /* events fired on the draggable target */
-  DaDZone.addEventListener('drag', drag, false);
-
   DaDZone.addEventListener('dragstart', dragstart, false);
-  DaDZone.addEventListener('touchstart', dragstart, false);
-
   DaDZone.addEventListener('dragend', dragend, false);
-  DaDZone.addEventListener('touchend', dragend, false);
-
   /* events fired on the drop targets */
   DaDZone.addEventListener('dragover', dragover, false);
-
   DaDZone.addEventListener('dragenter', dragenter, false);
-
   DaDZone.addEventListener('dragleave', dragleave, false);
-  DaDZone.addEventListener('touchleave', dragleave, false);
-
   DaDZone.addEventListener('drop', drop, false);
 }
 
+function selectedSource(event) {
+  let el = event.target;
+  document.querySelectorAll('div img').forEach((ele) => { 
+    ele.className = '';
+  });
+  if (el.className === 'selected') {
+    el.className = '';
+  } else {
+    if (selectedEl !== el) {
+      selectedEl = el;
+      el.className = 'selected';
+    } else {
+      selectedEl = null;
+    }
+  }
+}
+
+function selectedTarget(event) {
+  let el = event.target;
+  if (selectedEl) {
+    el.appendChild(selectedEl);
+    selectedEl.className = '';
+    selectedEl = null;
+  }
+}
+
+function mobileOption() {
+  const aSource = document.querySelectorAll('div[class="sourcezone"] img');
+  document.querySelectorAll('div[class="sourcezone"] img').forEach((ele) => {
+    ele.addEventListener('click', selectedSource, false);
+  });
+  document.querySelectorAll('div[class="dropzone"]').forEach((ele) => {
+    ele.addEventListener('click', selectedTarget, false);
+  });
+}
+
+function load() {
+  desktopOption();
+  mobileOption();
+}
+
 document.addEventListener('DOMContentLoaded', load, false);
+
+
+
+
